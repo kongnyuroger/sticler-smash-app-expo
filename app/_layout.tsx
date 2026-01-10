95713c428ee1dea9be656c9847e4ade8ab6917d6
Kongnyu Roger <kongnyuroger711@gmail.com>
2026-01-10 01:45:55 +0100

    leaned how to chenged color of the phone status bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,10 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
   return(
+    <>
+    < StatusBar style="light"/>
     <Stack>
       <Stack.Screen
         name="(tabs)"
@@ -12,5 +15,7 @@ export default function RootLayout() {
         options={{ title: "Oops!" }}/>
     </Stack>
 
+    </>
+    
   ) ;
 }
