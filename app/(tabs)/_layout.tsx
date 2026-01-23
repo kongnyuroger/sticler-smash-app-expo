af50b16a91385a896ef96f56d00b2935a089b269
Kongnyu Roger <kongnyuroger711@gmail.com>
2026-01-23 23:40:59 +0100

    builded justures

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,30 +1,47 @@
 import { Ionicons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function RootLayout() {
-  return(
-    <Tabs screenOptions={{
-      tabBarActiveTintColor:"#ffd33d",
-      headerStyle: { backgroundColor: "#25292e" },
-      headerTitleStyle: { color: "#fff" },
-      tabBarStyle: { backgroundColor: "#25292e" }
-    }}
-      
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-           headerTitle: "stiker smacher",
-           tabBarIcon: ({ focused, color, size }) => <Ionicons name={focused ? "home" : "home-outline"} size={size} color={color} />
-           }}
-      />
-      <Tabs.Screen
-        name="about"
-        options={{ title: "About",
-            tabBarIcon: ({ focused, color, size }) => <Ionicons name={focused ? "information-circle" : "information-circle-outline"} size={size} color={color} />
+  return (
+    <GestureHandlerRootView>
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: "#ffd33d",
+          headerStyle: { backgroundColor: "#25292e" },
+          headerTitleStyle: { color: "#fff" },
+          tabBarStyle: { backgroundColor: "#25292e" },
         }}
-        
-      />
-    </Tabs>
-  ) ;
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            headerTitle: "stiker smacher",
+            tabBarIcon: ({ focused, color, size }) => (
+              <Ionicons
+                name={focused ? "home" : "home-outline"}
+                size={size}
+                color={color}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="about"
+          options={{
+            title: "About",
+            tabBarIcon: ({ focused, color, size }) => (
+              <Ionicons
+                name={
+                  focused ? "information-circle" : "information-circle-outline"
+                }
+                size={size}
+                color={color}
+              />
+            ),
+          }}
+        />
+      </Tabs>
+    </GestureHandlerRootView>
+  );
 }
